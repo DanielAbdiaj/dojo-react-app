ae49dd4ff243641e1a092b1abb653bf522b896d3
DanielAbdiaj <117307377+DanielAbdiaj@users.noreply.github.com>
2023-03-17 23:15:29 +0100

    commit9

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,26 +1,26 @@
-
-import { useParams } from 'react-router-dom'
-import { useDocument } from '../../hooks/useDocument';
-
-
+import { useParams } from "react-router-dom"
+import { useDocument } from '../../hooks/useDocument'
 
 // styles
 import './Project.css'
+import ProjectComments from "./ProjectComments"
+import ProjectSummary from "./ProjectSummary"
 
 export default function Project() {
+  const { id } = useParams()
+  const { document, error } = useDocument('projects', id)
 
-const {id} =useParams();
-const {document,error}=useDocument('projects',id);
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>
+  }
 
-if(error){
-  return <div className="error">{error}</div>
-}
-if(!document){
-  return <div className="loading">Loading...</div>
-}
   return (
     <div className="project-details">
-      <h1>{document.name}</h1>
+      <ProjectSummary project={document} />
+      <ProjectComments project={document}/>
     </div>
   )
 }
